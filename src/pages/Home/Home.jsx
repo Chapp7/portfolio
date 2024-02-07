f0e62592af32e9dc8288e8d06f6171597cf98b8d
Prem <jampuramprem01@gmail.com>
2024-02-07 10:37:39 +0530

    added hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,12 +9,26 @@ const Home = () => {
           <Navbar />
         </div>
 
+        <div className="landing-page">
+          <div className="header-content">
+            <h1>Experienced Web Developer</h1>
+            <h2>Creating Innovative Solutions</h2>
+            <small>
+              Welcome to my website portfolio. I specialize in creating visually
+              stunning and user-friendly websites that drive results.
+            </small>
+            <div className="buttons">
+              <button className="contact-me">Contact Me</button>
+            </div>
+          </div>
+        </div>
+
         <div className="footer">
           <Footer />
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
